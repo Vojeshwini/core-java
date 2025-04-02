d96bdc5027b96401a8c41be3d4be8c11e3ee7625
VOJESHWINI MV <139997736+Vojeshwini@users.noreply.github.com>
2025-04-02 22:46:22 +0530

    pushing the code to git

--- Airline/src/Airline.java
+++ Airline/src/Airline.java
@@ -80,7 +80,8 @@ class AirlineRunner {
         Airline airline2 = new Airline(4151, "Indigo");
         Airline airline3 = new Airline(5461, "Indigo", "India", "London");
         Airline airline4= new Airline(8525,"air india","bengaluru","delhi",5000,50);
-        Airline airline5= new Airline(7525,"air india","delhi","bengaluru",5000,50);
+        Airline airline5= new Airline(7525,"air india","delhi","bengaluru",5000,50,230);
+        Airline airline6= new Airline(7525,"air india","delhi","bengaluru",5000,250,50,200,"2.00 am","4 am");
 
         airline1.airlineInfo();
         airline2.airlineInfo();
