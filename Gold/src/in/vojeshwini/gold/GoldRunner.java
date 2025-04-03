b2bea91b42a0c0b337085494bf0c53d370abf92a
VOJESHWINI MV <139997736+Vojeshwini@users.noreply.github.com>
2025-04-03 23:33:57 +0530

    pushing code to the git

---
+++ Gold/src/in/vojeshwini/gold/GoldRunner.java
@@ -0,0 +1,13 @@
+package in.vojeshwini.gold;
+
+public class GoldRunner {
+    public static void main(String[] args){
+      String name="kadga";
+        Gold gold=new Gold();
+
+        Bangal bangal=new Bangal(name,gold);
+        bangal.size();
+
+
+    }
+}
