26201b77d70d489f00c74209facd8a81dccf6c7f
VOJESHWINI MV <139997736+Vojeshwini@users.noreply.github.com>
2025-04-04 14:14:46 +0530

    pushing code to git

--- SolarSystem/src/SolarSystem.java
+++ SolarSystem/src/SolarSystem.java
@@ -81,29 +81,3 @@ public void Info(){
 
 }
 
-class SolarSystemRunner{
-    public static void main(String[] args) {
-        String planet1="Mercury";
-        String planet2="Venus";
-        String planet3="Earth";
-        String planet4="Mars";
-        String planet5="jupiter";
-        String planet6="staturn";
-        String planet7="uranus";
-        String planet8="nepture";
-        int distFromSunToEarth=25000;
-        String biggestStar="Sun";
-
-        SolarSystem solar1=new SolarSystem(planet1,planet2,planet3,planet4,planet5,planet6,planet7,planet8,distFromSunToEarth,biggestStar);
-        SolarSystem solar2=new SolarSystem(planet1,planet2,planet3,planet4,planet5,planet6,planet7,planet8,distFromSunToEarth);
-        SolarSystem solar3=new SolarSystem(planet1,planet2,planet3,planet4);
-        SolarSystem solar4=new SolarSystem(planet1,planet2,planet3,planet4,planet5,planet6,planet7);
-        SolarSystem solar5=new SolarSystem(planet1,planet2,planet3,planet4,planet5,planet6);
-        solar1.Info();
-        solar2.Info();
-        solar3.Info();
-        solar4.Info();
-        solar5.Info();
-
-    }
-}
