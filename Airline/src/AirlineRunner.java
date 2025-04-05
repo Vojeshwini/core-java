4aa507e16cf82c7d0e7c95d7a9ed6392e48c5b16
VOJESHWINI MV <139997736+Vojeshwini@users.noreply.github.com>
2025-04-05 06:57:32 +0530

    pushing the code to git

--- Airline/src/AirlineRunner.java
+++ Airline/src/AirlineRunner.java
@@ -4,9 +4,9 @@ public class AirlineRunner {
             Airline airline1 = new Airline(4151);
             Airline airline2 = new Airline(4151, "Indigo");
             Airline airline3 = new Airline(5461, "Indigo", "India", "London");
-            Airline airline4= new Airline(8525,"air india","bengaluru","delhi",5000,50);
-            Airline airline5= new Airline(7525,"air india","delhi","bengaluru",5000,50,230);
-            Airline airline6= new Airline(7525,"air india","delhi","bengaluru",5000,250,50,200,"2.00 am","4 am");
+            Airline airline4 = new Airline(8525, "air india", "bengaluru", "delhi", 5000, 50);
+            Airline airline5 = new Airline(7525, "air india", "delhi", "bengaluru", 5000, 50, 230);
+            Airline airline6 = new Airline(7525, "air india", "delhi", "bengaluru", 5000, 250, 50, 200, "2.00 am", "4 am");
 
             airline1.airlineInfo();
             airline2.airlineInfo();
@@ -15,5 +15,5 @@ public class AirlineRunner {
             airline5.airlineInfo();
 
 
-    }
+        }
 }
