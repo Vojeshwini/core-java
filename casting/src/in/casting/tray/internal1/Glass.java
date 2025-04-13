1e6d2f81cbe4394176bff5973f16c08b926b5d58
VOJESHWINI MV <139997736+Vojeshwini@users.noreply.github.com>
2025-04-13 21:10:15 +0530

    overriding

---
+++ casting/src/in/casting/tray/internal1/Glass.java
@@ -0,0 +1,15 @@
+package in.casting.tray.internal1;
+
+public class Glass extends Tray{
+    public Glass(){
+        System.out.println("running no args constructor");
+
+    }
+    @Override
+    public void color(){
+        System.out.println("running color in Glass");
+    }
+    public void Material()
+    {
+        System.out.println("running material in the Glass");
+    }}
