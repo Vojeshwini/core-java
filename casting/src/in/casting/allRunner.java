fb17687205b74e5b43235ed04258f6e82452cab9
VOJESHWINI MV <139997736+Vojeshwini@users.noreply.github.com>
2025-04-13 21:09:48 +0530

    overriding

---
+++ casting/src/in/casting/allRunner.java
@@ -0,0 +1,22 @@
+package in.casting;
+import in.casting.tray.internal1.*;
+
+public class allRunner {
+    public static void main(String[] args) {
+
+
+        Tray tray = new Tray();
+        tray.color();
+        Tray tray1 = new Glass();
+        tray1.color();
+
+        Glass glass2 = (Glass) tray1;//changin the datatype of tray to glass
+        glass2.
+        Glass glass1 = new Glass();
+        glass1.Material();
+        glass1.color();
+
+    }
+
+
+}
---
+++ casting/src/in/casting/tray/internal1/Tray.java
@@ -0,0 +1,10 @@
+package in.casting.tray.internal1;
+
+public class Tray {
+    public Tray(){
+        System.out.println("running no args constructor");
+    }
+    public void color(){
+        System.out.println("running color in tray");
+    }
+}
