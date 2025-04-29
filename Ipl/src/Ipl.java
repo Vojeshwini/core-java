5f17cf5b5276836aabb2e55a0c4c813df7d89871
VOJESHWINI MV <139997736+Vojeshwini@users.noreply.github.com>
2025-04-29 19:36:32 +0530

    Count the number of letters present in each words of a sentencee

---
+++ Ipl/src/Ipl.java
@@ -0,0 +1,11 @@
+public class Ipl {
+    public static void main(String[] args){
+      String input="rcb will win the match against csk";
+
+              String[] result=input.split(" ");
+              for(String results:result){
+                  System.out.print(results.length()+" ");
+              }
+
+    }
+}
