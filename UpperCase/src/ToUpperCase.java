7d6ecd90471407600eeb1dc754da721e43dbf624
VOJESHWINI MV <139997736+Vojeshwini@users.noreply.github.com>
2025-05-01 22:05:14 +0530

    to upperCase

---
+++ UpperCase/src/ToUpperCase.java
@@ -0,0 +1,7 @@
+public class ToUpperCase {
+    public static void main(String[] args){
+        String input="hello how are you";
+                String output=input.toUpperCase();
+        System.out.println(output);
+    }
+}
