6e44c10e0beefce0b91c8e35a0ffa6ac047efae4
VOJESHWINI MV <139997736+Vojeshwini@users.noreply.github.com>
2025-05-05 00:17:00 +0530

    IsPalindrome

--- functionalInterface/src/in/java/task/isPrime.java
+++ functionalInterface/src/in/java/task/isPrime.java
@@ -1,4 +1,6 @@
 package in.java.task;
+//Create a functional interface for checking if a number is prime. Use a lambda to test multiple numbers.
+
 @FunctionalInterface
 public interface isPrime {
     void checkIsPrime(int number);
--- functionalInterface/src/in/java/task/task1/IsPalindrome.java
+++ functionalInterface/src/in/java/task/task1/IsPalindrome.java
@@ -1,5 +1,5 @@
 package in.java.task.task1;
-
+//2.Write a functional interface that checks if a string is a palindrome. Test it using a lambda.
 @FunctionalInterface
 public interface IsPalindrome {
     void checkPalindrome();
