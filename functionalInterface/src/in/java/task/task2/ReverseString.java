3c23d951a48b4a66e0e20e690ae76f549538825d
VOJESHWINI MV <139997736+Vojeshwini@users.noreply.github.com>
2025-05-05 00:16:10 +0530

    reverseString

---
+++ functionalInterface/src/in/java/task/task2/ReverseString.java
@@ -0,0 +1,7 @@
+package in.java.task.task2;
+//.Use an anonymous class to implement a functional interface that reverses a string.
+
+@FunctionalInterface
+public interface ReverseString {
+    void string();
+}
---
+++ functionalInterface/src/in/java/task/task2/reverseRunner.java
@@ -0,0 +1,25 @@
+package in.java.task.task2;
+
+import javax.jws.soap.SOAPBinding;
+
+public class reverseRunner {
+    public static void main(String[] args) {
+
+    ReverseString reverseString=new ReverseString() {
+        @Override
+        public void string() {
+            String input="hello how are you";
+
+            char[] charecters=input.toCharArray();
+            for(int i= charecters.length-1;i>=0;i--){
+                System.out.print(charecters[i]);
+            }
+
+
+        }
+
+    };
+
+        reverseString.string();
+}
+}
