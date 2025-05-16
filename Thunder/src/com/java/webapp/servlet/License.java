91e0cc5af21679754481b11d4d3b0c42a36d4645
VOJESHWINI MV <139997736+Vojeshwini@users.noreply.github.com>
2025-05-16 10:59:13 +0530

    HTTPServlet

---
+++ Thunder/src/com/java/webapp/servlet/License.java
@@ -0,0 +1,34 @@
+package com.java.webapp.servlet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class License extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+        String name=servletRequest.getParameter("name");
+        String contact=servletRequest.getParameter("contact");
+
+        long n_contact=0;
+        if(contact!=null){
+            n_contact=Long.parseLong(contact);
+        }
+
+        String age=req.getParameter("age");
+        double n_age=0;
+        if(age!=null){
+            n_age=Double.parseDouble(age);
+        }
+        String type=req.getParameter("type");
+
+        System.out.println("forwording to jsp");
+
+        RequestDispatcher requestDispatcher=req.getRequestDispatcher("licenseSuccess.jsp");
+        requestDispatcher.forward(req,resp);
+    }
+}
