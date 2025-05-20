6bb4570e0c8a060ddce8a26df96195c9bf66081e
VOJESHWINI MV <139997736+Vojeshwini@users.noreply.github.com>
2025-05-20 13:11:03 +0530

    DTO and Service

---
+++ DTO/src/com/java/jsp/ServiceImpl/DonationImpl.java
@@ -0,0 +1,10 @@
+package com.java.jsp.ServiceImpl;
+
+import com.java.jsp.service.DonationService;
+
+public class DonationImpl implements DonationService {
+    @Override
+    public boolean save(DonationDto donationDto){
+        return true;
+    }
+}
---
+++ DTO/src/com/java/jsp/service/DonationService.java
@@ -0,0 +1,6 @@
+package com.java.jsp.service;
+import com.java.jsp.dto;
+
+public interface DonationService {
+    boolean save(donationDto donationDto)
+}
