e1acd7d0534fbe2a0df1b127891bb198e70cf79e
VOJESHWINI MV <139997736+Vojeshwini@users.noreply.github.com>
2025-05-28 00:05:17 +0530

    DTO and Service

---
+++ DTO/src/com/java/jsp/ServiceImpl/LaboratoryImpl.java
@@ -0,0 +1,12 @@
+package com.java.jsp.ServiceImpl;
+
+import com.java.jsp.dto.DonationDto;
+import com.java.jsp.dto.LaboratoryDto;
+import com.java.jsp.service.LaboratoryService;
+
+public class LaboratoryImpl implements LaboratoryService {
+    @Override
+    public boolean save(LaboratoryDto laboratoryDto) {
+        return true;
+    }
+}
