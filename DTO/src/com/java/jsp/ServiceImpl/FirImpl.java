34db490d1e98b72cd79d711969ed977c8d6508a0
VOJESHWINI MV <139997736+Vojeshwini@users.noreply.github.com>
2025-05-29 01:45:00 +0530

    DTO and Service

---
+++ DTO/src/com/java/jsp/ServiceImpl/FirImpl.java
@@ -0,0 +1,12 @@
+package com.java.jsp.ServiceImpl;
+
+import com.java.jsp.dto.DonationDto;
+import com.java.jsp.dto.FirDto;
+import com.java.jsp.service.FirService;
+
+public class FirImpl implements FirService {
+    @Override
+    public boolean save(FirDto firDto) {
+        return true;
+    }
+}
